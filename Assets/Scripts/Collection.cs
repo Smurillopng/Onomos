611156f3582fc835b0ee679c75b384fceeb9f596
Sérgio Murillo <smurillopng@gmail.com>
2025-06-23 18:17:15 -0300

    unity update

---
+++ Assets/Scripts/Collection.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Onomos
+{
+    public class Collection : MonoBehaviour
+    {
+        public Text onomoDescriptionText;
+        public GameObject player;
+        public TrackProperties playerOnomosList;
+        public Button[] buttons;
+        public Image selectedOnomoImage;
+
+        private void Awake()
+        {
+            foreach (var button in buttons)
+            {
+                button.interactable = false;
+            }
+        }
+
+        public void SelectOnomoInCollection(GameObject selected)
+        {
+            var onomo = playerOnomosList.GetChoices(player, selected);
+            selectedOnomoImage.sprite = player.GetComponentInChildren<SpriteRenderer>().sprite;
+            onomoDescriptionText.text = onomo.GetComponent<OnomoStatus>().collectionDescription;
+        }
+    }
+}
